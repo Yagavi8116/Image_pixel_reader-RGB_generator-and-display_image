d0e196187cfa7c00f1a8fb1c2a0d794c50e1f738
Yagavi8116 <yagavikarthikeyan@gmail.com>
2023-06-19 12:06:20 +0530

    update commit

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,37 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-class Pixel {
+class Pixel extends JPanel {
     BufferedImage image;
     int width;
     int height;
-
     int red, green, blue;
     int x, y;
 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        try {
+            Thread.sleep(10);
+        } catch (Exception e) {
+
+        }
+        repaint();
+        g2d.setColor(new Color(red, green, blue));
+        g2d.fillOval(x, y, 5, 10);
+
+    }
 
     public Pixel() {
+        setBounds(0, 0, 350, 200);
+        setVisible(true);
+        setLayout(null);
+        repaint();
         try {
             File input = new File("src/img.png");
             image = ImageIO.read(input);
             width = image.getWidth();
             height = image.getHeight();
-
             int count = 0;
 
             for (int i = 0; i < height; i++) {
@@ -34,16 +49,19 @@ class Pixel {
                     blue = c.getBlue();
                     x = i;
                     y = j;
-
+                    Graphics g = this.getGraphics();
                 }
             }
 
         } catch (Exception e) {
         }
     }
+
 }
 
 public class Main extends JFrame {
+    private static Pixel pixel;
+
     Main() {
         super("Paint");
         setResizable(false);
@@ -52,7 +70,11 @@ public class Main extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    static public void main(String args[]) {
-        Pixel obj = new Pixel();
+    public static void main(String[] args) {
+        Main paint = new Main();
+        paint.setLocationRelativeTo(null);
+
+        pixel = new Pixel();
+        paint.add(pixel);
     }
 }
